faf55c8e792fe6cafc6fbf25ac9a62e31fb3ad81
Kamil Kordecki <kamil@kordecki.dev>
2024-05-22 18:50:21 +0200

    extension: Add some other Brave extensions

--- home/programs/browsers.nix
+++ home/programs/browsers.nix
@@ -7,8 +7,17 @@
     brave = {
       enable = true;
       extensions = [
-        {id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";} # ublock-origin
-      ];
+        {id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";} # uBlock Origin
+	{id = "hjdoplcnndgiblooccencgcggcoihigg";} # ToS; Didn't read 
+	{id = "gebbhagfogifgggkldgodflihgfeippi";} # Return Dislike Button
+	{id = "khncfooichmfjbepaaaebmommgaepoid";} # Unhook YouTube	      
+	{id = "fmkadmapgofadopljbjfkapdkoienihi";} # React Developer Tools	      
+	{id = "ohaiaoliglhljiamagbagenmmcdfgkfe";} # Glassdoor Bypass	      
+	{id = "mnjggcdmjocbbbhaepdhchncahnbgone";} # SponsorBlock	      
+	{id = "cmeikcgfecnhojcbfapbmpbjgllklcbi";} # Hive AI Detector	      
+	{id = "ponfpcnoihfmfllpaingbgckeeldkhle";} # Enchancer for  YouTube	      
+	{id = "bmanlajnpdncmhfkiccmbgeocgbncfln";} # Slinky Elegancki
+];
     };
 
   };
