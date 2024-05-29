02ce3d6145b93fe7d8b8934ba2247e3a11f91aa1
Kamil Kordecki <kamil@kordecki.dev>
2024-05-30 01:05:05 +0200

    dev: Add c/c++ tools

--- home/editors/packages.nix
+++ home/editors/packages.nix
@@ -7,6 +7,19 @@
 
   home.packages = with pkgs; [
 
+    #-- c/c++
+    cmake
+    cmake-language-server
+    gnumake
+    checkmake
+    # c/c++ compiler, required by nvim-treesitter!
+    gcc
+    # c/c++ tools with clang-tools, the unwrapped version won't
+    # add alias like `cc` and `c++`, so that it won't conflict with gcc
+    llvmPackages.clang-unwrapped
+    lldb
+
+
     #-- python --#
     nodePackages.pyright # python language server
     (python311.withPackages (
